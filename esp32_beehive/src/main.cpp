e0ddb6cf3995aab11a0238d8b4c40421a41afb2c
Vivek Kumar <vvkkumar06@gmail.com>
2025-09-21 23:32:40 +0530

    new changes

--- esp32_beehive/src/main.cpp
+++ esp32_beehive/src/main.cpp
@@ -1,29 +1,26 @@
+
 #include <WiFi.h>
-#include "BeehiveMonitor.h"
+#include <BeehiveMonitor.h>
+#include "secrets.h"
 
-// WiFi credentials
-const char* WIFI_SSID = "K-1003";
-const char* WIFI_PASSWORD = "Ashajha_123";
+BeehiveMonitor beehive(2, DHT11, 18, 19, 27, 26);
 
-BeehiveMonitor beehive(2, DHT11);
-
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to WiFi");
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("\nWiFi connected!");
+    Serial.println("🚀 Beehive Monitor Starting...");
+    beehive.initializeModem("");
 
     // Set APEX server details
-    beehive.setApexServer("oracleapex.com", 443, "/ords/beehive/api/data");
+    beehive.setApexServer(APEX_SERVER_HOST, APEX_SERVER_PORT, APEX_SERVER_PATH);
+
+    delay(3000); // Wait for modem to stabilize
 
     beehive.begin();
 }
 
-void loop() {
-    beehive.sendData(1, 6.2);
+void loop()
+{
+    beehive.sendData(BEEHIVE_ID);
     delay(5000);
 }
---
+++ gsm-module-impl/src/last-working-code.cpp
@@ -0,0 +1,83 @@
+// #define TINY_GSM_MODEM_SIM7600
+// #include <Arduino.h>
+// #include <TinyGsmClient.h>
+// #include <ArduinoHttpClient.h>
+
+// #define SerialAT Serial1
+// #define SerialMon Serial
+// #define RXD2 27
+// #define TXD2 26
+// #define powerPin 4
+
+// const char apn[] = "";  // leave blank for auto-detect
+// const char smsNumber[] = "+919667165303";
+// const char url[] = "https://beehive-mac-check.netlify.app/.netlify/functions/checkWhitelist?mac=00:4B:12:2F:E5:CC";
+
+// TinyGsm modem(SerialAT);
+// TinyGsmClient client(modem);
+// HttpClient http(client, "beehive-mac-check.netlify.app", 443);
+
+// void sendSMS(String msg) {
+//   SerialAT.println("AT+CMGF=1");
+//   delay(500);
+//   SerialAT.print("AT+CMGS=\"");
+//   SerialAT.print(smsNumber);
+//   SerialAT.println("\"");
+//   delay(500);
+//   SerialAT.print(msg);
+//   delay(500);
+//   SerialAT.write(26); // CTRL+Z
+//   delay(5000);
+// }
+
+// void setup() {
+//   Serial.begin(115200);
+//   // pinMode(powerPin, OUTPUT);
+//   // digitalWrite(powerPin, LOW);
+//   // delay(100);
+//   // digitalWrite(powerPin, HIGH);
+//   // delay(1000);
+//   // digitalWrite(powerPin, LOW);
+
+//   SerialAT.begin(115200, SERIAL_8N1, RXD2, TXD2);
+//   delay(10000);
+
+//   Serial.println("Initializing modem...");
+//   if (!modem.init() || !modem.restart()) {
+//     Serial.println("Modem init failed");
+//     return;
+//   }
+
+//   Serial.println("Waiting for network...");
+//   if (!modem.waitForNetwork()) {
+//     Serial.println("Network not found");
+//     return;
+//   }
+
+//   Serial.print("Connecting to GPRS...");
+//   if (!modem.gprsConnect(apn)) {
+//     Serial.println(" failed");
+//     return;
+//   }
+//   Serial.println(" success");
+
+//   Serial.println("Making HTTPS request...");
+//   http.beginRequest();
+//   http.get("/.netlify/functions/checkWhitelist?mac=00:4B:12:2F:E5:CC");
+//   http.endRequest();
+
+//   int status = http.responseStatusCode();
+//   String resp = http.responseBody();
+
+//   Serial.println("HTTP status: " + String(status));
+//   Serial.println("Response: " + resp);
+
+//   // String msg = (resp.indexOf("true") != -1) ? "Authorized" : "Unauthorized";
+//   // sendSMS(msg);
+
+//   Serial.println("Done!");
+// }
+
+// void loop() {
+//   // nothing here
+// }
